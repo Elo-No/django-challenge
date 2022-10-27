1ea47567b9c5a39a591891800ff73c427eb5831f
Elo-No <eleno.nouri@gmail.com>
2022-10-27 18:46:11 +0330

    Fix message body

--- core/v1/permissions/has_federation_permission.py
+++ core/v1/permissions/has_federation_permission.py
@@ -3,14 +3,14 @@ from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 
 
 class IsFederation(IsAuthenticated):
-    message = 'You have access.'
+    message = 'You have not access.'
 
     def has_permission(self, request: Request, view):
         return super().has_permission(request, view) and request.user.is_federation_agent
 
 
 class IsFederationOrReadOnly(IsFederation):
-    message = 'You have access.'
+    message = 'You have not access.'
 
     def has_permission(self, request: Request, view):
         return request.method in SAFE_METHODS or super().has_permission(request, view)
