2b51f521fbe09595922f17ade4061502411119b4
Elo-No <eleno.nouri@gmail.com>
2022-10-27 17:44:42 +0330

    Add new fields

--- account/models.py
+++ account/models.py
@@ -82,6 +82,10 @@ class CustomUser(AbstractBaseUser, BaseModel):
         unique=True,
     )
 
+    is_federation_agent = models.BooleanField(
+        verbose_name=_('Is federation agent?'),
+        default=False,
+    )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
