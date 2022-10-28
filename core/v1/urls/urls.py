2a795657c69f2af13686af10cad488f868379bd5
Elo-No <eleno.nouri@gmail.com>
2022-10-28 11:02:20 +0330

    Fix end points

--- core/v1/urls/urls.py
+++ core/v1/urls/urls.py
@@ -6,7 +6,7 @@ from ..views.tickt_view import BuyTicktAPIView, ListSeatAPIView
 urlpatterns = [
     path("stadium/", CreateStadiumAPIView.as_view(), name="create_stadium"),
     path("match/", CreateMatchAPIView.as_view(), name="create_match"),
-    path('match/available-seat/', ListSeatAPIView.as_view(), name="availble-seats"),
-    path("match/buy/<int:pk>/", BuyTicktAPIView.as_view(), name="buy_tickt"),
+    path('match/available-seat/', ListSeatAPIView.as_view(), name="availble_seats"),
+    path("match/buy-tickt/", BuyTicktAPIView.as_view(), name="buy_tickt"),
 
 ]
--- federation/urls.py
+++ federation/urls.py
@@ -4,18 +4,12 @@ from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from django.contrib import admin
 from django.urls import path, include, re_path
-...
-
-...
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Volleyball Federation API",
         default_version='v1',
         description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
